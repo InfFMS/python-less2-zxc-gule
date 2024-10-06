131a1125eb653f9ccdaea2ae8e6abde83819b1ae
Янчик <yalovikyan2006@gmail.com>
2024-10-06 15:19:20 +0800

    Исправил 11 задание

--- task11.py
+++ task11.py
@@ -7,18 +7,18 @@ n=0
 if a>=c and b<=d:
     m=a
     n=b
-elif (c>=a and d<=b):
+elif c>=a and d<=b:
     m=c
     n=d
-elif a>=c and d<=b:
+elif a>=c and d<=b and c<=b:
     m=a
     n=d
-elif a<=c and d>=b:
+elif a<=c and d>=b and b>=c:
     m=c
     n=b
-elif (a<=c and b<=c) or (a>=d and b>=d):
+if (a<c and b<c) or (a>d and b>d):
     print("пустое множество")
-if m!=n:
+elif m!=n:
     print('['+str(m)+','+str(n)+']')
 else:
     print('['+str(m)+']')
